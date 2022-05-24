83181751210888410cf8c1a19ecce7f000a56a22
Justin Freeman <justin@agileware.com.au>
2022-05-24 17:31:55 +1000

    PROJ-2254 Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Change Job Title to Select List
+
+Changes the Contact, Job Title field to a select list of options, removing the free-text field.
+This will restrict which options users can select from and does not change stored values already in this field.
+
+_Note_: When the user edits a contact, any saved values that are _not_ in the Job Title option group will _not_ be shown in the select list.
+
+## Installation and configuration
+
+1. Install and enable this CiviCRM extension.
+2. When this Extension is installed a new option group, Job Title is created.
+3. Go to the `Administer CiviCRM > Option Groups` page.
+4. Add the Job Title options that you want used in CiviCRM.
+
+# About the Authors
+
+This CiviCRM extension was developed by the team at
+[Agileware](https://agileware.com.au).
+
+[Agileware](https://agileware.com.au) provide a range of CiviCRM services
+including:
+
+* CiviCRM migration
+* CiviCRM integration
+* CiviCRM extension development
+* CiviCRM support
+* CiviCRM hosting
+* CiviCRM remote training services
+
+Support your Australian [CiviCRM](https://civicrm.org) developers, [contact
+Agileware](https://agileware.com.au/contact) today!
+
+
+![Agileware](logo/agileware-logo.png)
---
+++ selectjobtitle.php
@@ -0,0 +1,139 @@
+<?php
+
+require_once 'selectjobtitle.civix.php';
+
+// phpcs:disable
+use Civi\Api4\OptionGroup;
+use Civi\Api4\OptionValue;
+use CRM_Selectjobtitle_ExtensionUtil as E;
+
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function selectjobtitle_civicrm_config(&$config) {
+  _selectjobtitle_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function selectjobtitle_civicrm_install() {
+  selectjobtitle_setup_optiongroups();
+
+  _selectjobtitle_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function selectjobtitle_civicrm_postInstall() {
+  _selectjobtitle_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function selectjobtitle_civicrm_uninstall() {
+  _selectjobtitle_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function selectjobtitle_civicrm_enable() {
+  selectjobtitle_setup_optiongroups();
+
+  _selectjobtitle_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function selectjobtitle_civicrm_disable() {
+  _selectjobtitle_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function selectjobtitle_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _selectjobtitle_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function selectjobtitle_civicrm_entityTypes(&$entityTypes) {
+  _selectjobtitle_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Create the required Activity Category and Activity Category, CiviMobile
+ * option
+ */
+
+function selectjobtitle_setup_optiongroups() {
+  // Check that the Activity Category Option exists
+  $optionGroups = OptionGroup::save(FALSE)->addRecord([
+    'name'        => 'job_title',
+    'title'       => 'Job Title',
+    'description' => 'Job Title options',
+    'data_type'   => 'String',
+    'is_active'   => TRUE,
+  ])->setMatch(['name'])->execute();
+
+  $optionValues = OptionValue::save(FALSE)->addRecord([
+    'option_group_id.name' => 'job_title',
+    'label'                => 'Sanitation Officer',
+    'value'                => 'Sanitation Officer',
+    'name'                 => 'Sanitation Officer',
+  ])->setMatch(['value'])->execute();
+}
+
+/**
+ * Implements hook_civicrm_buildForm().
+ */
+function selectjobtitle_civicrm_buildForm($formName, &$form) {
+
+  // Display category option for activity types and activity statuses.
+  if ($formName == 'CRM_Contact_Form_Contact' || $formName == 'CRM_Contact_Form_Inline_ContactInfo' || $formName == 'CRM_Contact_Form_Search_Advanced') {
+    $options = civicrm_api3('optionValue', 'get', [
+      'option_group_id' => 'job_title',
+      'is_active'       => 1,
+      'options'         => ['limit' => 0, 'sort' => 'weight'],
+    ]);
+    $opts    = [];
+
+    foreach ($options['values'] as $opt) {
+      $opts[] = [
+        'id'   => $opt['name'],
+        'text' => $opt['label'],
+      ];
+    }
+
+    $form->add('select2', 'job_title', ts('Job Title'), $opts, FALSE, [
+      'class'    => 'crm-select2',
+      'multiple' => FALSE,
+    ]);
+  }
+}
